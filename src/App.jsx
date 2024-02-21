3b253e986c519b1200ff2e623b68690a6bb45352
Johanna Köllner <johanna.a.koellner@gmail.com>
2024-02-21 13:56:43 +0100

    added contact data

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ function App() {
     <>
       <h1>Agathe Köllner</h1>
       <h2>Energetik und Mediation</h2>
-      <p>Seite in Arbeit.</p>
+      <p>Seite in Arbeit</p>
+      <p>Terminvereinbarung unter info@agathekoellner.at oder 0699/12734270</p>
+      <p>Hauptstraße 119/GL4,<br/>3021 Pressbaum</p>
     </>
   )
 }
